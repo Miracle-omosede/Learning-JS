c82080feae24c084356fa83b1224b8c242b0e5d4
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-06-28 18:29:27 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,6 @@
+let name = "Miracle"
+let whatDoYouWannaBecome = "Software Engineer"
+let gender = "male"
+let twitterHandle = "www.twitter.com/IgbinogheneM"
+
+console.table({ name, whatDoYouWannaBecome, gender, twitterHandle})
