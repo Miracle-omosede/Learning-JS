5ef88fd76d90b8d87e1fb48a11b47426907e31ee
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-07-06 07:11:17 +0100

    case ND switch statements

--- style.js
+++ style.js
@@ -1,50 +1,9 @@
-// a task to create and store variables in javascript
+let day = ('monday'.toUpperCase)
 
-let name = "Miracle"
-let whatDoYouWannaBecome = "Software Engineer"
-let gender = "male"
-let twitterHandle = "www.twitter.com/IgbinogheneM"
-
-// console.table({ name, whatDoYouWannaBecome, gender, twitterHandle})
-
-
-// Reserved Keywords
-// Variables
-// Data types
-//  Boolean data types
-
-// null can be set by a programmer while undefined can be set by the browser...
-
-// Starting a new sequence
-
-// JavaScript Slice 
-// console.log(name.split(""))
-
-// Learning arrays
-const arrayOfStuffs = ['M', 'O', 'U', 'S', 'E']
-
-console.log(arrayOfStuffs.join(""))
-// join methods in JavaScript...
-
-console.log(arrayOfStuffs[0])
-
-console.log(arrayOfStuffs.includes("O"))
-
-
-// trim
-
-let favActorFirstname = "Chris"
-let favActorLastname = "Hemsworth"
-let fullname = `${favActorFirstname} ${favActorLastname}`
-
-let uppercase = (fullname.toUpperCase())
-
-let message = `My favourite actor is ${uppercase}, `;
-message += `His best TV show is silicon valley`;
-
-console.log(message)
-
-
-
-// we're gonna be learning constructor 
-
+switch (day) {
+    case "monday":
+    console.log("Today is monday")
+    break;
+    default:
+        console.log("don't knw what day is today")
+}
